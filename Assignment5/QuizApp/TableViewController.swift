ce5d1a89f2ce2e49acce925ed541bcd46b0ed9b8
amarino <amarino@eecs.wsu.edu>
2019-02-07 02:18:45 -0800

    Finished all necessary code; however, it needs to be cleaned up and tested further.

--- Assignment5/QuizApp/TableViewController.swift
+++ Assignment5/QuizApp/TableViewController.swift
@@ -10,58 +10,85 @@ import UIKit
 
 class TableViewController: UITableViewController {
     var questions = [Question]();
+    //var basicCells = [UITableViewCell]();
     var selectedRow: Int = 0;
     @IBOutlet var questionsTable: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        questionsTable.delegate = self;
-        questionsTable.dataSource = self;
+        self.questionsTable.isHidden = true;
+        self.questionsTable.delegate = self;
+        self.questionsTable.dataSource = self;
+        //self.questionsTable.beginUpdates()
         for i in 0..<Prompt.count{
-            questions.append(Question(QuizPrompt: Prompt[i],
+            self.questions.append(Question(QuizPrompt: Prompt[i],
                                       Answers: Answers[i],
                                       CorrectAnswer: CorrectAnswers[i]));
+            //basicCells.append(.init(style: .default, reuseIdentifier: Prompt[i]))
+            //let row = IndexPath.init(row: i, section: 0);
+            //self.questionsTable.insertRows(at: [row], with: .automatic);
+            //let cell = self.questionsTable.cellForRow(at: row);
+            //cell?.textLabel?.text = Prompt[i];
         }
+        //self.questionsTable.endUpdates()
+        self.questionsTable.isHidden = false;
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
-
-    // MARK: - Table view data source
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true;
+    }
+    // Override to support editing the table view.
     override func tableView(_ tableView: UITableView,
-                            didSelectRowAt indexPath: IndexPath) {
+                            commit editingStyle: UITableViewCell.EditingStyle,
+                            forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source first
+            self.questions.remove(at: indexPath.row);
+            self.questionsTable.deleteRows(at: [indexPath], with: .fade)
+        } }
+    // MARK: - Table view data source
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         self.selectedRow = indexPath.row // retain for prepareForSegue
-        performSegue(withIdentifier: "toDetail", sender: nil)
+        performSegue(withIdentifier: "DisplayQuestionSegue", sender: nil)
     }
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1;
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        //return self.basicCells.count;
+        return self.questions.count;
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if (segue.identifier == "DisplayQuestionSegue") {
+            let DisplayQuestionView = segue.destination as! ViewController;
+            DisplayQuestionView.question = self.questions[self.selectedRow];
+        }
+        else if (segue.identifier == "AddQuestionSegue") {
+            let AddQuestionView = segue.destination as! AddQuestionViewController;
+            AddQuestionView.questionNum = self.questions.count;
+        }
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
+        let cell = tableView.dequeueReusableCell(withIdentifier:
+            "questionCell", for: indexPath);
+        let question = self.questions[indexPath.row];
+        cell.textLabel?.text? = question.QuizPrompt;
+        return cell;
     }
-    */
+    
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
 
     /*
     // Override to support editing the table view.
--- Assignment5/QuizApp/ViewController.swift
+++ Assignment5/QuizApp/ViewController.swift
@@ -15,6 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var Answer3: UILabel!
     @IBOutlet weak var Answer4: UILabel!
     @IBOutlet weak var Answer5: UILabel!
+    var Answers = [UILabel]();
     @IBOutlet weak var QuestionPrompt: UILabel!;
     @IBOutlet weak var StackView: UIStackView!
     override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
@@ -23,51 +24,39 @@ class ViewController: UIViewController {
             StackView.distribution = .equalSpacing;
             StackView.axis = .horizontal;
             let font = UIFont.boldSystemFont(ofSize: 16)
-            Answer1.font = font;
-            Answer2.font = font;
-            Answer3.font = font;
-            Answer4.font = font;
-            Answer5.font = font;
+            for i in 0..<self.Answers.count{
+                self.Answers[i].font = font;
+            }
         } else {
             StackView.spacing = 15.0;
             StackView.axis = .vertical;
             StackView.alignment = .leading;
             let font = UIFont.boldSystemFont(ofSize: 25)
-            Answer1.font = font;
-            Answer2.font = font;
-            Answer3.font = font;
-            Answer4.font = font;
-            Answer5.font = font;
+            for i in 0..<self.Answers.count{
+                self.Answers[i].font = font;
+            }
         }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        Answer1.isHidden = true;
-        Answer2.isHidden = true;
-        Answer3.isHidden = true;
-        Answer4.isHidden = true;
-        Answer5.isHidden = true;
+        self.Answers = [Answer1, Answer2, Answer3, Answer4, Answer5];
+        for i in 0..<self.Answers.count{
+            self.Answers[i].isHidden = true;
+        }
         loadNewQuestion();
     }
     
     func loadNewQuestion(){
         QuestionPrompt.text = self.question!.QuizPrompt;
         let answerCount: Int = self.question!.Answers.count;
+        let letters: [String] = ["a", "b", "c", "d", "e"];
         for i in 0..<answerCount{
             let answer: String = self.question!.Answers[i];
-            switch i{
-            case 0: Answer1.text = "a.) \(answer)";
-                Answer1.isHidden = false;
-            case 1: Answer2.text = "b.) \(answer)";
-                Answer2.isHidden = false;
-            case 2: Answer3.text = "c.) \(answer)";
-                Answer3.isHidden = false;
-            case 3: Answer4.text = "d.) \(answer)";
-                Answer4.isHidden = false;
-            case 4: Answer5.text = "e.) \(answer)";
-                Answer5.isHidden = false;
-            default: break;
+            self.Answers[i].text = "\(letters[i]).) \(answer)";
+            if(self.question?.CorrectAnswer == i){
+                self.Answers[i].textColor = .red;
             }
+            self.Answers[i].isHidden = false;
         }
     }
 }
